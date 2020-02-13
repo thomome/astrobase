169a7ef0e1a12615289c8a51e2b518a2969bd9d8
Thomas Guthruf <thomasguthruf@gmail.com>
2020-02-14 00:00:16 +0100

    created icon component

---
+++ components/AbIcon.vue
@@ -0,0 +1,33 @@
+<template>
+	<svg
+		:class="['icon', name]"
+	>
+		<use
+			v-bind="{'xlink:href' : link}"
+		/>
+	</svg>
+</template>
+
+<script>
+export default {
+	props: {
+		name: { type: String, required: true }
+	},
+	computed: {
+		link () {
+			return `/symbol-defs.svg#icon-${this.name}`
+		}
+	}
+}
+</script>
+
+<style>
+.icon {
+	display: inline-block;
+	width: 1em;
+	height: 1em;
+	stroke-width: 0;
+	stroke: currentColor;
+	fill: currentColor;
+}
+</style>
