bcf350c05df8f709116be5099cd2c1b6b19efa85
Thomas <thomasguthruf@gmail.com>
2020-12-05 12:46:50 +0100

    added moving average

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -15,4 +15,27 @@ function ceilMinutes (value, minutes = 5) {
 	return Math.ceil(value / 60 / minutes) * 60 * minutes
 }
 
-export { clamp, ceilMinutes, floorMinutes }
+function movingAverage (array, prop = null, window = 5) {
+	return array.map((obj, index) => {
+		let sum = 0
+		let count = 0
+		for (let i = (window - 1) * -0.5; i <= (window - 1) * 0.5; i++) {
+			const o2 = array[index + i]
+			const value = prop && o2 ? o2[prop] : o2
+			if (!isNaN(value)) {
+				sum += value
+				count++
+			}
+		}
+		const avg = count > 0 ? sum / count : 0
+		if (prop) {
+			obj[prop] = avg
+		} else {
+			obj = avg
+		}
+
+		return obj
+	})
+}
+
+export { clamp, ceilMinutes, floorMinutes, movingAverage }
