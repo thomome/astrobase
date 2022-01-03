99093b20c7bc6711c646eba10239cbe6334b2200
Thomas <thomasguthruf@gmail.com>
2022-01-04 00:51:00 +0100

    add more info to grid item

--- components/AbGalleryPicture.vue
+++ components/AbGalleryPicture.vue
@@ -10,12 +10,26 @@
 					:sizes="sizes"
 					class="picture__image w-full h-full object-cover"
 				/>
-				<div class="picture__info absolute w-full text-gray-200 bottom-0 left-0 px-3 pb-2 pt-20 bg-gradient-to-t from-black to-transparent">
-					<h4 class="font-medium text-gray-200 leading-tight text-lg">
+				<div class="picture__info">
+					<div class="flex justify-between">
+						<div v-if="modes.length > 0" class="picture__text a-top d-3">
+							<ab-tag
+								v-for="mode in modes"
+								:key="mode.value"
+								:label="mode.label"
+								:type="mode.value"
+								class="sm"
+							/>
+						</div>
+						<div v-if="totalExposureTime" class="picture__text a-top d-4">
+							<span class="text-sm text-gray-200" v-html="totalExposureTime" />
+						</div>
+					</div>
+					<h4 class="picture__text a-top d-2 font-medium text-gray-200 leading-tight text-xl mt-auto">
 						{{ picture.title }}
 					</h4>
-					<div class="text-gray-500 text-sm leading-relaxed">
-						{{ picture.date }}
+					<div class="picture__text a-top d-1 text-gray-500 text-sm leading-relaxed">
+						{{ picture.date }}<span v-if="picture.location[0]"> - {{ picture.location[0].title }}</span>
 					</div>
 				</div>
 			</nuxt-link>
@@ -24,6 +38,7 @@
 </template>
 
 <script>
+import moment from 'moment'
 import AbImage from '~/components/AbImage.vue'
 
 export default {
@@ -35,6 +50,35 @@ export default {
 	computed: {
 		image () {
 			return this.picture.image[0]
+		},
+		modes () {
+			const { exposures } = this.picture
+			const index = {}
+			const modes = []
+			exposures.forEach((exp) => {
+				if (!index[exp.mode.value]) {
+					index[exp.mode.value] = true
+					modes.push(exp.mode)
+				}
+			})
+			return modes
+		},
+		totalExposureTime () {
+			const secs = this.picture.exposures.reduce((p, v) => {
+				return p + v.exposure_time * v.amount
+			}, 0)
+
+			const duration = moment.duration(secs * 1000)
+			const hours = duration.hours()
+			const minutes = duration.minutes()
+			const seconds = duration.seconds()
+
+			let durationString = ''
+			if (hours) { durationString += `${hours}&#8239;<small>h</small> ` }
+			if (minutes) { durationString += `${minutes}&#8239;<small>min</small> ` }
+			if (seconds) { durationString += `${seconds}&#8239;<small>s</small>` }
+
+			return durationString
 		}
 	}
 }
@@ -43,22 +87,42 @@ export default {
 <style lang="scss" scoped>
 	.picture {
 		.picture__info {
+			position: absolute;
+			display: flex;
+			flex-direction: column;
+			left: 0;
+			top: 0;
+			width: 100%;
+			height: 100%;
+			padding: 1rem;
 			opacity: 0;
+			background: linear-gradient(0deg, #000000cc, #00000066);
 			transition: opacity 0.25s;
+		}
 
-			* {
-				transform: translateY(-10px);
-				transition: transform 0.25s;
+		.picture__text {
+			opacity: 0;
+			transition: transform 0.4s, opacity 0.5s;
+
+			&.a-top {
+				transform: translateY(-20px);
+			}
+
+			@for $i from 1 through 6 {
+				&.d-#{$i} {
+					transition-delay: #{$i * 0.05}s;
+				}
 			}
 		}
 
 		&:hover {
 			.picture__info {
 				opacity: 1;
+			}
 
-				* {
-					transform: translateY(0);
-				}
+			.picture__text {
+				transform: translate(0, 0);
+				opacity: 1;
 			}
 		}
 	}
